b6dfa20aaf2bbf70f91e377a8344c2aee2bad2a7
Anthony Mapes, Jr <anthonymapes3@gmail.com>
2017-06-21 23:36:32 -0400

    Reads map CSV and corrects the book name appropriately

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+import csv
 from textwrap import TextWrapper
 import xml.etree.ElementTree as ET
 
@@ -9,6 +10,9 @@ import xml.etree.ElementTree as ET
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--infile", help="The input file to read.  Must be in Zefania XML format.")
 parser.parse_args()
+
+## TODO ask for a CSV names list
+correctNamesFile = 'names.csv'
 
 ## TODO get file names from args
 inFileName = 'src.xml'
@@ -21,11 +25,20 @@ wrapper = TextWrapper(width=79, )
 tree = ET.parse(inFileName)
 root = tree.getroot()
 
+# Create a dict of the names from the CSV
+correctNamesReader = csv.DictReader(open(correctNamesFile))
+correctNamesDict = dict()
+for row in correctNamesReader:
+	correctNamesDict[row['XMLName']] = row['RealName']
+
 for book in root:
 	# Iterating over the root's tags, most of which are books
 	if book.tag == "BIBLEBOOK":
 		# Now we know that it's actually a book
 		bookName = book.get('bname')
+		if bookName in correctNamesDict:
+			bookName = correctNamesDict[bookName]
+
 		# TODO have a dictionary of the Zefania name to the real name
 		for chapter in book:
 			if chapter.tag == "CHAPTER":
@@ -37,7 +50,7 @@ for book in root:
 						# TODO write out the verse to the output file,
 						# wrapped to 80 lines
 						wrappedText = wrapper.fill(verseContent)
-						print(wrappedText)
+						#print(wrappedText)
 						citeLine = "        --" + bookName + " " + chapterNum + ":" + verseNum
-						print(citeLine)
-		#print("Completed " + bookName)
+						#print(citeLine)
+		print(bookName)
